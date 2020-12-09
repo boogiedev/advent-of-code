78779dc27d33d0b46a35acef76242089076e3751
Wesley Nguyen <nguyenw@seattleu.edu>
2020-12-08 19:56:16 -0800

    first day

---
+++ solution.py
@@ -0,0 +1,25 @@
+# Imports
+
+
+# Global Functions
+
+
+
+
+
+
+def day_1_solution(expense_report:list) -> int:
+    '''
+    --- Day 1: Report Repair ---
+
+    Given an expense report (a list of integers), find the two entries that sum to 2020 and multiply both numbers together.
+
+    Example:
+
+    [1721, 979, 366, 299, 675, 1456]
+
+    This expense report shows that 1721 (idx 0) and 299 (idx 3) sum up to 2020. The solution for this is 1721 * 299 -> 514579
+    '''
+
+
+day_1_ipt = [1721, 979, 366, 299, 675, 1456]
